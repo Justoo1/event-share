9d00d49c8580de0c01f41f6a80c383107794d84e
Justoo1 <amankrahjay@gmail.com>
2024-08-30 10:14:43 +0000

    Implement stripe

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'utfs.io',
+            port: '',        // Leave empty if no specific port is required
+            pathname: '/**', // Allows all paths under utfs.io
+          },
+        ],
+    },
+};
 
 export default nextConfig;
